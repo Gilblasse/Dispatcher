e6e2bfa1487b0bcb610e19387a15132ec0d266f2
gilblasse <gilblasse@gmail.com>
2022-03-09 22:39:35 -0500

    Fixed DB Unhandled Err

--- functions/config.js
+++ functions/config.js
@@ -1,20 +1,10 @@
-const firebase = require('firebase')
-const {log} = require("firebase-functions/lib/logger");
+const admin = require("firebase-admin");
 
-log(process.env)
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: process.env.AGMT_FIREBASE_API_KEY,
-    authDomain: "agmtlambdaapi.firebaseapp.com",
-    projectId: "agmtlambdaapi",
-    storageBucket: "agmtlambdaapi.appspot.com",
-    messagingSenderId: "419079393490",
-    appId: "1:419079393490:web:b49013dc060a7284321d0b",
-    measurementId: "G-NV89QSFP0J"
-};
+admin.initializeApp();
+const settings = { timestampsInSnapshots: true };
+admin.firestore().settings(settings);
+const db = admin.firestore();
 
-firebase.initialize(firebaseConfig)
-const db = firebase.firestore()
 const TripsDb = db.collection("trips");
 
 module.exports = TripsDb
